1352255b794ca953d432bae3271148ac7717c594
ilevy <itayill01@gmail.com>
2024-06-15 12:49:47 +0300

    Emit prologue and epilogue

--- compilation.py
+++ compilation.py
@@ -1,6 +1,29 @@
+from blocks import Operation, Assignment
+
 class Compiler:
+    PROLOGUE = """
+.global main
+main:
+push %rbp
+mov  %rsp, %rbp
+"""
+    EPILOGUE = """
+mov %rax, %rsi
+lea  format(%rip), %rdi
+call printf
+
+mov %rbp, %rsp
+pop %rbp
+ret
+
+format:
+.asciz "%d\\n"
+"""
     def __init__(self, output):
         self.output = output
+        self.stack_positions = {}
+        self.stack_top = 0
 
-    def compile(self):
-        pass
+    def compile(self, blocks):
+        self.output.write(self.PROLOGUE)
+        self.output.write(self.EPILOGUE)
