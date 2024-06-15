44d65ee5e808692c2b867169f991b4d0cb33803e
ilevy <itayill01@gmail.com>
2024-06-15 12:50:08 +0300

    Assemble output

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import click
 import sys
+import subprocess
 
 from compilation import Compiler
 from tokenization import Tokenizer
@@ -8,17 +9,19 @@ from errors import CompilationError
 
 @click.command()
 @click.argument("path")
-@click.option("-o", "--output", default="a.out")
-def main(path: str, output: str):
+@click.option("-o", "--output", "bin_out", default="a.out")
+def main(path: str, bin_out: str):
+    asm_out = bin_out + '.S'
     with open(path) as file:
         content = file.read()
     try:
         tokens = Tokenizer().tokenize(content)
-        with open(output,'w') as output:
+        with open(asm_out ,'w') as output:
             compiler = Compiler(output)
-            compiler.compile()
+            compiler.compile(tokens)
     except CompilationError as e:
         print(f"{path}:{e}", file=sys.stderr)
+    subprocess.run(['gcc', asm_out, '-o', bin_out])
 
 
 if __name__ == "__main__":
