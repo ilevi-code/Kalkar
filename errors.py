db51a75cc70c338fd5581c124c11d2d332924599
ilevy <itayill01@gmail.com>
2024-06-15 13:18:15 +0300

    Fix error highlighting offset

--- errors.py
+++ errors.py
@@ -2,12 +2,13 @@ from position import Position
 
 class CompilationError(Exception):
     def __init__(self, position: Position, highlight_len: int, message: str):
+        import pdb; pdb.set_trace()
         self.position = position
         self.highlight_len = highlight_len
         self.message = message
 
     def __str__(self):
-        space_pad = " " * self.position.offset
+        space_pad = " " * self.position.offset - 1
         highlight = "^" * self.highlight_len
         return f"{self.position.line_number}: {self.message}\n{self.position.line}\n{space_pad}{highlight}"
 
