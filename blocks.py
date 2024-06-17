a4dca435abeb5841daff9fb8cecd9c0feb840a5f
ilevi <itayill01@gmail.com>
2024-06-18 00:41:10 +0300

    Better parenthesis handling

--- blocks.py
+++ blocks.py
@@ -2,7 +2,6 @@ from tokenization import Operator
 
 class BinaryOperation:
     ORDER_OF_OPERATIONS = {
-        "(": 3,
         "*": 2,
         "/": 2,
         "+": 1,
@@ -13,19 +12,23 @@ class BinaryOperation:
         self.lhs = lhs
         self.operator = operator
         self.rhs = rhs
-        self.order = self.ORDER_OF_OPERATIONS[operator.operator]
+        self.order = BinaryOperation.ORDER_OF_OPERATIONS[operator.operator]
+        self.is_parenthseized = False
+
+    def is_lower_order(self, other):
+        return other.order < self.order and not other.is_parenthseized
 
     def parenthesize(self):
-        self.order = self.ORDER_OF_OPERATIONS["("]
+        self.is_parenthseized = True
         return self
 
     def reorder(self):
-        if type(self.lhs) is BinaryOperation and self.lhs.order < self.order:
+        if type(self.lhs) is BinaryOperation and self.is_lower_order(self.lhs):
             new_root = self.lhs
             self.lhs = new_root.rhs
             new_root.rhs = self
             return new_root
-        if type(self.rhs) is BinaryOperation and self.rhs.order < self.order:
+        if type(self.rhs) is BinaryOperation and self.is_lower_order(self.rhs):
             new_root = self.rhs
             self.rhs = new_root.lhs
             new_root.lhs = self
@@ -38,6 +41,7 @@ class BinaryOperation:
             and self.operator == other.operator
             and self.rhs == other.rhs
             and self.order == other.order
+            and self.is_parenthseized == other.is_parenthseized
         )
 
     def __str__(self):
